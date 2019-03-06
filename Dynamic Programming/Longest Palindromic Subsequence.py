04b3afa8e1ebda229678ae6b10603ab9345c7195
Mahmood Al Rayhan <MhmdRyhn@users.noreply.github.com>
2019-03-07 00:29:57 +0600

    Longest Palindromic Subsequence

---
+++ Dynamic Programming/Longest Palindromic Subsequence.py
@@ -0,0 +1,69 @@
+/*
+    ** Make all substring of length 1 as palindrome
+        for i = 0 to string_length-1
+            board[i][i] = 1;
+    ** Now, use the following conditions to evaluate for all the substrings of length > 1
+        if string[i] == string[i+substring_length-1]:
+            board[i][i+substring_length-1] = 2 + board[i+1][i+substring_length-2]
+        else:
+            board[i][i+substring_length-1] = max(board[i+1][i+substring_length-1], board[i][i+substring_length-2])
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    // freopen("in.txt", "r", stdin);
+
+    string str;
+
+    cin>>str;
+
+    int len;
+    len = str.length();
+
+    int board[len][len];
+    // pair<int, int> source[len][len];
+
+    for(int i=0; i<len; i++)
+        board[i][i] = 1;
+
+    for(int t=2; t<=len; t++)
+    {
+        for(int i=0; (i+t-1)<len; i++)
+        {
+            if(str[i] == str[i+t-1])
+            {
+                board[i][i+t-1] = 2;
+                if(t>2)
+                {
+                    board[i][i+t-1] += board[i+1][i+t-2];
+                    // source[i][i+t-1] = {i+1, i+t-2};
+                }
+            }
+            else
+            {
+                board[i][i+t-1] = max(board[i+1][i+t-1], board[i][i+t-2]);
+                /*
+                if(board[i+1][i+t-1] >= board[i][i+t-2])
+                    source[i][i+t-1] = {i+1, i+t-1};
+                else
+                    source[i][i+t-1] = {i, i+t-2};
+                */
+            }
+        }
+    }
+
+    cout<<"Longest palindromic subsequence length: "<<board[0][len-1]<<endl;
+
+    /// Using the source[][], it's easy to get the `Longest Palindromic Subsequence`
+
+    return 0;
+}
+
